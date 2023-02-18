eef19a35f5d31f5ad220cce2c15aff129d4bb305
sgrsaga <sgrsagara@gmail.com>
2023-02-18 14:38:09 +0530

    Completely new terrastack

---
+++ eks_al2.tf
@@ -0,0 +1,42 @@
+module "eks_al2" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 19.5"
+
+  cluster_name    = "${local.name}-al2"
+  cluster_version = "1.24"
+
+  # EKS Addons
+  cluster_addons = {
+    coredns    = {}
+    kube-proxy = {}
+    vpc-cni    = {}
+  }
+
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
+
+  eks_managed_node_groups = {
+    al2 = {
+      instance_types = ["m6i.large", "m5.large", "m5n.large", "m5zn.large"]
+      capacity_type  = "SPOT"
+
+      block_device_mappings = {
+        xvda = {
+          device_name = "/dev/xvda"
+          ebs = {
+            volume_size = 75
+            volume_type = "gp3"
+            iops        = 3000
+            throughput  = 150
+          }
+        }
+      }
+
+      min_size     = 1
+      max_size     = 3
+      desired_size = 1
+    }
+  }
+
+  tags = module.tags.tags
+}
---
+++ eks_bottlerocket.tf
@@ -0,0 +1,33 @@
+module "eks_bottlerocket" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 19.5"
+
+  cluster_name    = "${local.name}-br"
+  cluster_version = "1.24"
+
+  # EKS Addons
+  cluster_addons = {
+    coredns    = {}
+    kube-proxy = {}
+    vpc-cni    = {}
+  }
+
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
+
+  eks_managed_node_groups = {
+    bottlerocket = {
+      ami_type = "BOTTLEROCKET_x86_64"
+      platform = "bottlerocket"
+
+      instance_types = ["m6i.large", "m5.large", "m5n.large", "m5zn.large"]
+      capacity_type  = "SPOT"
+
+      min_size     = 1
+      max_size     = 3
+      desired_size = 1
+    }
+  }
+
+  tags = module.tags.tags
+}
